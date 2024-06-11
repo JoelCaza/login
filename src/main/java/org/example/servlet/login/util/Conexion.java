0fdbdf2c81587830eb86d7744435b879cc0fb0de
Joel <joelcaza@hotmail.com>
2024-06-11 09:37:18 -0500

    Base de datos con sd

--- src/main/java/org/example/servlet/login/util/Conexion.java
+++ src/main/java/org/example/servlet/login/util/Conexion.java
@@ -6,35 +6,11 @@ import java.sql.SQLException;
 import java.sql.SQLOutput;
 
 public class Conexion {
+    private static String url = "jdbc://localhost:3306/mydb?serverTimezone=America/Quito";
+    private static String usuario = "root";
+    private static String password = "1234";
 
-
-    public static Connection getConnection() {
-
-        Connection connexion = null;
-        var baseDatos = "mydb";
-        var url = "jdbc:mysql://localhost:3306/" + baseDatos;
-        var usuario = "root";
-        var password = "1234";
-        //Cargamos el pool de conexiones al driver en memoria
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
-            connexion = DriverManager.getConnection(url, usuario, password);
-        }
-        catch (ClassNotFoundException | SQLException e) {
-            System.out.println("Ocurrio un error al conectase a la base de datos"+e.getMessage());
-
-        }
-        return connexion;
-
-    }
-
-    public static void main (String[] args) {
-        Connection connexion =getConnection();
-        if (connexion == null) {
-            System.out.println("Error al conectar a la base de datos");
-        }
-        else {
-            System.out.println("Conexion Exitosa");
-        }
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(url, usuario, password);
     }
 }
